c2f4003fc09ea2811df2b895348f2a2866da96d7
SherwinPedro <shesheshe024@gmail.com>
2022-08-27 23:03:44 +0800

    edit

--- formscript.js
+++ formscript.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const form = document.querySelector("form"),
         nextBtn = form.querySelector(".nextBtn"),
         backBtn = form.querySelector(".backBtn"),
@@ -16,22 +15,3 @@ nextBtn.addEventListener("click", ()=> {
 })
 
 backBtn.addEventListener("click", () => form.classList.remove('secActive'));
-=======
-const form = document.querySelector("form"),
-        nextBtn = form.querySelector(".nextBtn"),
-        backBtn = form.querySelector(".backBtn"),
-        allInput = form.querySelectorAll(".first input");
-
-
-nextBtn.addEventListener("click", ()=> {
-    allInput.forEach(input => {
-        if(input.value != ""){
-            form.classList.add('secActive');
-        }else{
-            form.classList.remove('secActive');
-        }
-    })
-})
-
-backBtn.addEventListener("click", () => form.classList.remove('secActive'));
->>>>>>> ba10b50882443b6a905a1418f232e3f08dd0c445
