0aaa652cd44b7b74b63cf71b8fc567023ac3f7e6
ruthcuasay <ruthcuasay28@gmail.com>
2022-09-02 00:42:48 +0800

    FORDA 30% HAHAAHHAHA

--- sql/info.sql
+++ sql/info.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 27, 2022 at 06:41 AM
+-- Generation Time: Sep 01, 2022 at 04:31 PM
 -- Server version: 5.6.12-log
 -- PHP Version: 5.4.16
 
@@ -28,6 +28,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pic` blob NOT NULL,
   `fname` text NOT NULL,
   `lname` text NOT NULL,
   `age` int(11) NOT NULL,
@@ -48,18 +49,15 @@ CREATE TABLE IF NOT EXISTS `info` (
   `contact` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `info`
 --
 
-INSERT INTO `info` (`id`, `fname`, `lname`, `age`, `bday`, `gender`, `address`, `height`, `weight`, `shot`, `vit`, `meds`, `medhis`, `pfname`, `plname`, `civstatus`, `relate_child`, `guardian_add`, `contact`) VALUES
-(1, 'sherwin', 'sdjkfbskdjfb', 234, '0234-04-23', 'Male', 'sdfjbaskdfb', 345, 34, 'sdhfb', 'hsfbjh', 'hbhjb', 'hjb', 'sherwion', 'jkbsdfjk', 'Single', 'sdjkfbs', 'kj', 34),
-(2, 'sherwin', 'sdjnfjksb', 234, '0023-04-23', 'Female', 'sdjfnskjd', 34, 34, 'sdjfbskjd', 'jkdbfgksjd', 'jbdfkjsb', 'jbsdjkb', 'asbfhjbd', 'sdhfbsjdhb', 'Single', 'djbfjhsb', 'hsdfbhjsdsd', 34235),
-(3, 'she', 'sdfjgkj', 34, '0043-03-31', 'Male', 'dkjfbkgjdsb', 345, 54, 'dfdjnfkj', 'jdbfsdkjb', 'jdfbjksdb', 'jkbsdfjksdb', 'jbsdfb', 'kdjksfbks', 'Single', 'nasfb', 'bsdfkjb', 34523),
-(4, 'as', 'as', 12, '0012-12-12', 'Male', '12', 12, 12, '12', '12', '12', '12', '12', '12', 'Single', '12', '12', 12),
-(5, 'as', 'as', 12, '0012-12-12', 'Male', '12', 12, 12, '12', '12', '12', '12', '12', '12', 'Single', '12', '12', 12);
+INSERT INTO `info` (`id`, `pic`, `fname`, `lname`, `age`, `bday`, `gender`, `address`, `height`, `weight`, `shot`, `vit`, `meds`, `medhis`, `pfname`, `plname`, `civstatus`, `relate_child`, `guardian_add`, `contact`) VALUES
+(1, 0x312e6a7067, 'Ianna', 'Cuasay', 13, '2009-05-14', 'Female', 'San Pablo City, Laguna', 157, 39, 'Hepatitis B.', 'Vitamin C.', 'none', 'Pet Allergy', 'Marilyn', 'Cuasay', 'Married', 'Mother', 'San Pablo City, Laguna', 2147483647),
+(2, 0x352d312d38313978313032342e6a7067, 'Julia', 'Cuasay', 13, '2009-03-25', 'Female', 'Brgy. Del Remedio San Pablo City, Laguna', 150, 42, 'Hepatitis B.', 'Vitamin A.', 'Amoxicillin', 'Sinus Infections', 'Numeriano', 'Cuasay', 'Married', 'Father', 'Brgy. Del Remedio San Pablo City, Laguna', 999556789);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
